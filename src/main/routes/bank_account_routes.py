ea621cc2b6699b55b49c99d4a152fce6e858de84
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-05-28 15:05:48 -0300

    Updating

--- src/main/routes/bank_account_routes.py
+++ src/main/routes/bank_account_routes.py
@@ -5,6 +5,8 @@ from src.main.composer.user_register_composer import user_register_composer
 from src.main.composer.login_creator_composer import login_creator_composer
 from src.main.composer.balance_editor_composer import balance_editor_composer
 
+from src.main.middlewares.auth_jwt import auth_jwt_verify
+
 bank_route_bp = Blueprint('bank_route', __name__)
 
 @bank_route_bp.route("/bank/registry", methods=["POST"])
@@ -21,6 +23,7 @@ def create_login():
 
 @bank_route_bp.route("/bank/balance/<user_id>", methods=["PATCH"])
 def edit_balance(user_id):
-    http_request = HttpRequest(body=request.json, params={"user_id": user_id})
+    token_information = auth_jwt_verify()
+    http_request = HttpRequest(body=request.json, params={"user_id": user_id}, token_infos=token_information, headers= request.headers)
     http_response = balance_editor_composer().handle(http_request)
     return jsonify(http_response.body), http_response.status_code
